81df5d9426e2ccaa0540637d94e20d1fb3265e7d
Lukas Steffen <lukas@stable.steffen-steffen.ch>
2020-11-05 17:36:32 +0100

    Add method imports to __init__.py

--- xmas_elves/__init__.py
+++ xmas_elves/__init__.py
@@ -1,2 +1,6 @@
-#Provide methods and classes from other files in the same directory.
-#from .xmas_elves import _method_
+# Provide methods and classes from other files in the same directory.
+# from .xmas_elves import _method_
+
+from .xmaselves import send_xmas_mails
+from .xmaselves import draw_graph
+from .xmaselves import resend_mail_for_node
